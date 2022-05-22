2633f5d8667511c5d9bf5f72b01a1626828664ac
Denys Matsenko <Denys.Matsenko@davita.com>
2022-05-22 17:15:47 +0300

    Add db authentication

--- src/main/java/com/github/saintukrainian/patientdashboard/security/SecurityConfig.java
+++ src/main/java/com/github/saintukrainian/patientdashboard/security/SecurityConfig.java
@@ -1,34 +1,43 @@
 package com.github.saintukrainian.patientdashboard.security;
 
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private static final String DOCTOR_ROLE = "DOCTOR";
-  private static final String ADMIN_ROLE = "ADMIN";
+  private static final String DOCTOR_ROLE = "ROLE_DOCTOR";
+  private static final String ADMIN_ROLE = "ROLE_ADMIN";
+
+  private final DataSource dataSource;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    UserBuilder users = User.withDefaultPasswordEncoder();
-
-    auth.inMemoryAuthentication()
-        .withUser(users.username("Bill_Jackson").password("bill123").roles(DOCTOR_ROLE))
-        .withUser(users.username("Administrator").password("admin").roles(DOCTOR_ROLE, ADMIN_ROLE));
+    auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .usersByUsernameQuery("select username, password, enabled "
+            + "from users"
+            + " where username = ?")
+        .authoritiesByUsernameQuery("select username, authority "
+            + "from authorities"
+            + " where username = ?");
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
         .antMatchers("/**")
-        .hasRole(DOCTOR_ROLE)
+        .hasAuthority(DOCTOR_ROLE)
         .and()
         .formLogin()
         .and()
@@ -36,4 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .permitAll();
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
